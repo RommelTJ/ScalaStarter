db86239dd6db8fef96071c92c1d33a95d893b22f
Rommel Rico <me@rommelrico.com>
2018-10-28 13:14:10 -0700

    Adding a website title

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -10,6 +10,8 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
+  private var websiteTitle: String = _
+
   /**
    * Create an Action to render an HTML page with a welcome message.
    * The configuration in the `routes` file means that this method
@@ -17,7 +19,7 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * a path of `/`.
    */
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(websiteTitle))
   }
 
 }

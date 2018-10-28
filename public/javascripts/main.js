7a133f93331071dcbb4e35a63acf6e2e439b71f9
Rommel Rico <me@rommelrico.com>
2018-10-28 15:59:49 -0700

    Displaying the title with Ajax

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,9 +7,24 @@ $("#urlInputForm").submit(function(e) {
         url: url,
         data: form.serialize(), // serializes the form's elements.
         success: function(data) {
-            console.log("TITLE: " + data.title);
+            if (data != null && data.title != null && data.title !== "") {
+                var title = data.title;
+                insertAlert(title);
+            }
         }
     });
 
+
+
     e.preventDefault(); // avoid to execute the actual submit of the form.
 });
+
+function insertAlert(title) {
+    $("#chatmeter-alert-section").empty();
+
+    var htmlString = "<div class='alert alert-primary chatmeter-alert' role='alert'>";
+    htmlString += title;
+    htmlString += "</div>";
+
+    $("#chatmeter-alert-section").html(htmlString);
+}

feb356b9715a7c532330acaa14340b4088be5f77
Rommel Rico <me@rommelrico.com>
2018-10-28 18:08:51 -0700

    Comments on main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,6 @@
+/**
+ * AJAX submission of the URL.
+ */
 $("#urlInputForm").submit(function(e) {
     var form = $(this);
     var url = form.attr('action');
@@ -29,7 +32,9 @@ $("#urlInputForm").submit(function(e) {
     e.preventDefault(); // avoid to execute the actual submit of the form.
 });
 
-
+/**
+ * Binding to the inputURL field so we can clear the alert if the inputForm field is empty.
+ */
 $("#inputURL").bind('keyup', function (e) {
     var myInput = $("#inputURL").val();
     if (myInput != null && myInput === "") {
@@ -37,6 +42,10 @@ $("#inputURL").bind('keyup', function (e) {
     }
 });
 
+/**
+ * Inserts a title under the form.
+ * @param title
+ */
 function insertAlert(title) {
     $("#chatmeter-alert-section").empty();
 
@@ -47,6 +56,11 @@ function insertAlert(title) {
     $("#chatmeter-alert-section").html(htmlString);
 }
 
+/**
+ * Big Regex that handles most cases of valid URLs. Returns true if url is valid, false otherwise.
+ * @param url
+ * @returns {boolean}
+ */
 function isUrlValid(url) {
     return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
 }
